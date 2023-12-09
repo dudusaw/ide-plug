95b28b73ae8e2ac61250d02fba0c18e666a6bd8e
GitHub Action <action@github.com>
2023-12-09 18:38:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/dudusaw/ideplug/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/dudusaw/ideplug/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dudusaw.ideplug.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
